d617dfe85212c15722965a41512d229487eebb2c
Gleb Prokopchuk <r0938719@ucll.be>
2023-02-21 12:13:10 +0100

    Commit

--- 01-basic-python/01-arithmetic/03-cakes/student.py
+++ 01-basic-python/01-arithmetic/03-cakes/student.py
@@ -1,1 +1,7 @@
 # Write your code here
+def cakes(eggs, butter, flour):
+    eggs  = eggs // 5
+    butter = butter // 250
+    flour = flour // 250
+    list_of_ingredients = [eggs, butter, flour]
+    return min(list_of_ingredients)
--- 01-basic-python/03-loops/01-digit-sum/student.py
+++ 01-basic-python/03-loops/01-digit-sum/student.py
@@ -1,1 +1,13 @@
 # Write your code here
+def last_digit(digit):
+    return digit % 10
+
+def remove_last_digit(digit):
+    return digit // 10
+
+def digit_sum(digit):
+    sum = 0
+    while digit > 0:
+        sum += last_digit(digit)
+        digit = remove_last_digit(digit)
+    return sum
