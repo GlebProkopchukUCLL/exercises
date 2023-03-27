f59bde3083af80cfc06303904973d8d60db2d601
Gleb Prokopchuk <r0938719@ucll.be>
2023-03-27 11:16:25 +0200

    update

---
+++ 04-dictionaries/01-assoc-list/student.py
@@ -0,0 +1,8 @@
+class AssocList:
+    def __init__(self):
+        self.__items = []
+    def __setitem__(self, key, value):
+        pass
+    def __getitem__(self, key):
+        pass
+assoc_list = AssocList()
